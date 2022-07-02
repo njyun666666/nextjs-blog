52ecdca9cb2305ed26e04b47f1d5574faacd1bb1
Jyun <njyun666666@gmail.com>
2022-07-02 13:39:02 +0800

    Layout Component, CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Head from "next/head";
 import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -25,6 +26,6 @@ export default function FirstPost() {
           Hello World
         </a>
       </Link>
-    </>
+    </Layout>
   );
 }
