34f72b4ef4290ee2b16ed09eef9a703916c9590c
Jyun <njyun666666@gmail.com>
2022-07-03 12:59:35 +0800

    classnames

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,25 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import cn from "classnames";
 
 export default function Home() {
+  const type = "success"; // success error
+
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p
+          className={cn({
+            [utilStyles.success]: type === "success",
+            [utilStyles.error]: type === "error",
+          })}
+        >
+          [Your Self Introduction]
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
